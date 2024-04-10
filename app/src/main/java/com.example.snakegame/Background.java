b6557b79d1df2ad92114b4c41094ccdbc3d4ba99
gotecho <tonymike71@gmail.com>
2024-04-10 12:54:32 -0700

    Update Background.java
    Added functionality for the background to be scaled to any screen size

--- app/src/main/java/com.example.snakegame/Background.java
+++ app/src/main/java/com.example.snakegame/Background.java
@@ -5,21 +5,30 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.WindowManager;
+import android.util.DisplayMetrics;
 
 public class Background extends GameObject implements Drawable{
-    private final int height = 1100; // Screen height in pixels
-    private final int width = 3000; // Screen width in pixels
     Background(Context context) {
         super(context);
         setBitmap(loadAndScaleResource(context, R.drawable.background));
     }
+
     // Function: Draw the pause button
     @Override
     public void draw(Canvas canvas, Paint paint) {
         canvas.drawBitmap(getBitmap(), 0, 0, paint);
     }
 
+
     private Bitmap loadAndScaleResource(Context context, int resourceId) {
+        // Get size of any device's screen using the application
+        DisplayMetrics disMet = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(disMet);
+        int height = disMet.heightPixels;
+        int width = disMet.widthPixels;
+
         Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), resourceId);
         return Bitmap.createScaledBitmap(bitmap, width, height, false);
     }
