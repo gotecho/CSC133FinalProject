3f2866159316a8f04a34c00c399332992bc331ad
KevCendana <kevincendana@csus.edu>
2024-04-28 12:43:26 -0700

    PowerUp now abstract, PowerUpScoreDoubler
    Files Changed: SnakeGame.java, PowerUp.java, PowerUpScoreDoubler.java
    
    - SnakeGame now has new variables: scoreMultiplier, scoreMultiplierCounter. They also have setters and getters.
    - PowerUp is now an abstract class to follow better design patterns.
    - Each PowerUp must implement the abstract method activate() since each one has different functionality.
    - Renamed ScoredDoubler.java -> PowerUpScoreDoubler.java to be cleaner.
    - PowerUpScoreDoubler now has functionality and gives the next 2 apple a bonus score.

---
+++ app/src/main/java/com.example.snakegame/PowerUpScoreDoubler.java
@@ -0,0 +1,19 @@
+package com.example.snakegame;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.util.Log;
+
+public class PowerUpScoreDoubler extends PowerUp {
+
+    // Constructor: Called when the PowerUp class is first created
+    PowerUpScoreDoubler(SnakeGame snakeGame, Context context, Point spawnRange, int size){
+        super("Score Doubler", snakeGame, context, spawnRange, size, R.drawable.scoredoubler);
+    }
+
+    @Override
+    public void activate() {
+        Log.d("print-log", "PowerUpScoreDoubler.activate(): Activated Score Doubler power up.");
+        snakeGame.setScoreMultiplier(2);
+    }
+}
