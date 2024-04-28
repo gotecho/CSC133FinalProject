a8ca06108ed7822199e66c530e904ceeebb1fb5e
KevCendana <kevincendana@csus.edu>
2024-04-28 12:43:26 -0700

    PowerUp now abstract, PowerUpScoreDoubler
    Files Changed: SnakeGame.java, PowerUp.java, PowerUpScoreDoubler.java
    
    - SnakeGame now has new variables: scoreMultiplier, scoreMultiplierCounter. They also have setters and getters.
    - PowerUp is now an abstract class to follow better design patterns.
    - Each PowerUp must implement the abstract method activate() since each one has different functionality.
    - Renamed ScoredDoubler.java -> PowerUpScoreDoubler.java to be cleaner.
    - PowerUpScoreDoubler now has functionality and gives the next 2 apple a bonus score.

--- app/src/main/java/com.example.snakegame/PowerUp.java
+++ app/src/main/java/com.example.snakegame/PowerUp.java
@@ -10,18 +10,21 @@ import android.util.Log;
 
 import java.util.Random;
 
-public class PowerUp extends GameObject implements Drawable, Collidable {
+public abstract class PowerUp extends GameObject implements Drawable, Collidable {
     private final Point location = new Point(); // Location of power up in grid (Not in pixels)
     private final Point mSpawnRange;            // Range of values we can choose from to spawn a power up
     private final int mSize;                    // Size of the power up
     private int score;                          // Score the player gets when they eat the power up
     private String name;                        // Name of the power up
     private boolean isVisible;                  // Flag to track if the power up is visible
+    protected SnakeGame snakeGame;                     // Reference to the game
+    public abstract void activate();            // Abstract method to activate the power up
 
     // Constructor: Called when the PowerUp class is first created
-    PowerUp(String name, Context context, Point spawnRange, int size, int drawableId){
+    PowerUp(String name, SnakeGame snakeGame, Context context, Point spawnRange, int size, int drawableId){
         super(context);             // Call the constructor of the GameObject class
         this.name = name;           // Initialize the name of the power up
+        this.snakeGame = snakeGame; // Initialize the reference to the game
         mSpawnRange = spawnRange;   // Initialize the spawn range
         mSize = size;               // Initialize the size of the power up
         location.x = -10;           // Initialize the location of the power up
@@ -36,7 +39,7 @@ public class PowerUp extends GameObject implements Drawable, Collidable {
     // Function: Spawn a PowerUp
     void spawn(){
         // Choose two random values and place the power up
-        Log.d("print-log", "PowerUp.spawn()");
+        Log.d("print-log", "PowerUp.spawn(): Spawned a " + name + " power up.");
         this.isVisible = true;
         Random random = new Random();
         location.x = random.nextInt(mSpawnRange.x) + 1;
@@ -48,7 +51,6 @@ public class PowerUp extends GameObject implements Drawable, Collidable {
     @Override
     public void draw(Canvas canvas, Paint paint){
         if (!isVisible) return;
-        Log.d("print-log", "PowerUp.draw()");
         canvas.drawBitmap(getBitmap(), location.x * mSize, location.y * mSize, paint);
     }
 
